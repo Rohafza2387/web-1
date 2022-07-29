3151b4f64612c204fce6edd23eee075dd2cec515
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:43:45 +0430

    Update delete-user.php

--- admin/delete-user.php
+++ admin/delete-user.php
@@ -1,1 +1,28 @@
+<?php 
 
+ 
+    include('../config/constants.php');
+
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM tbl_user WHERE id=$id";
+
+    $res = mysqli_query($conn, $sql);
+
+    if($res==true)
+    {
+
+        $_SESSION['delete'] = "<div class='success'>Admin Deleted Successfully.</div>";
+
+        header('location:'.SITEURL.'admin/manage-users.php');
+    }
+    else
+    {
+
+        $_SESSION['delete'] = "<div class='error'>Failed to Delete Admin</div>";
+        header('location:'.SITEURL.'admin/manage-users.php');
+    }
+
+  
+
+?>
