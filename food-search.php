f0e9659837558759884ad288dc37af8622c7ae8b
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:20:45 +0430

    Update food-search.php

--- food-search.php
+++ food-search.php
@@ -1,56 +1,35 @@
 
     <?php include('partials-front/menu.php'); ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
-        
             <?php 
-
-                //Get the Search Keyword
                 $search = $_POST['search'];
-            
             ?>
 
 
-            <h2><a href="#" class="text-white">Foods on Your Search "<?php echo $search; ?>"</a></h2>
+    <h2><a href="#" class="text-white">Foods on Your Search "<?php echo $search; ?>"</a></h2>
 
-    
-    <!-- fOOD sEARCH Section Ends Here -->
-
-
-
-    <!-- fOOD MEnu Section Starts Here -->
     <section class="food-menu">
         <div class="container">
                
         <?php 
-
-//Get the Search Keyword
             $search = $_POST['search'];
 
 ?>
-
-
             <h2><a href="#" class="text-center">Foods on Your Search "<?php echo $search; ?>"</a></h2>
     
 
             <?php 
-
-                //SQL Query to Get foods based on search keyword
                 $sql = "SELECT * FROM tbl_food WHERE title LIKE '%$search%' ";
 
-                //Execute the Query
                 $res = mysqli_query($conn, $sql);
 
-                //Count Rows
                 $count = mysqli_num_rows($res);
 
-                //Check whether food available of not
                 if($count>0)
                 {
-                    //Food Available
                     while($row=mysqli_fetch_assoc($res))
                     {
-                        //Get the details
+
                         $id = $row['id'];
                         $title = $row['title'];
                         $price = $row['price'];
@@ -60,15 +39,12 @@
                         <div class="food-menu-box">
                             <div class="food-menu-img">
                                 <?php 
-                                    // Check whether image name is available or not
                                     if($image_name=="")
                                     {
-                                        //Image not Available
                                         echo "<div class='error'>Image not Available.</div>";
                                     }
                                     else
                                     {
-                                        //Image Available
                                         ?>
                                         <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
                                         <?php 
@@ -92,21 +68,15 @@
                 }
                 else
                 {
-                    //Food Not Available
                     echo "<div class='error'>Food not found.</div>";
                 }
             
             ?>
 
-            
-
-            <div class="clearfix"></div>
-
-            
+            <div class="clearfix"></div>     
 
         </div>
 
     </section>
-    <!-- fOOD Menu Section Ends Here -->
 
     
