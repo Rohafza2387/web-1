0b4acccf80e06ee54fe35d8bb29b0c48232c0e99
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:33:54 +0430

    Update order.php

--- order.php
+++ order.php
@@ -1,43 +1,27 @@
 
 <?php include('partials-front/menu.php'); ?>
 
+<?php 
 
-    <?php 
-
-        
-        if(isset($_SESSION['login']))
+    if(isset($_SESSION['login']))
         {
     echo $_SESSION['login'];
     unset($_SESSION['login']);
 }
 
-
-
-
-
-
-
-
-
-
-
-        //CHeck whether food id is set or not
-        if(isset($_GET['food_id']))
+    if(isset($_GET['food_id']))
         {
-            //Get the Food id and details of the selected food
             $food_id = $_GET['food_id'];
 
-            //Get the DEtails of the SElected Food
             $sql = "SELECT * FROM tbl_food WHERE id=$food_id";
-            //Execute the Query
+
             $res = mysqli_query($conn, $sql);
-            //Count the rows
+
             $count = mysqli_num_rows($res);
-            //CHeck whether the data is available or not
+
             if($count==1)
             {
-                //WE Have DAta
-                //GEt the Data from Database
+
                 $row = mysqli_fetch_assoc($res);
 
                 $title = $row['title'];
@@ -46,19 +30,15 @@
             }
             else
             {
-                //Food not Availabe
-                //REdirect to Home Page
                 header('location:'.SITEURL);
             }
         }
         else
         {
-            //Redirect to homepage
             header('location:'.SITEURL);
         }
     ?>
 
-    <!-- fOOD sEARCH Section Starts Here -->
     <section class="food-search2">
         <div class="container">
             
@@ -70,16 +50,12 @@
 
                     <div class="food-menu-img">
                         <?php 
-                        
-                            //CHeck whether the image is available or not
                             if($image_name=="")
                             {
-                                //Image not Availabe
                                 echo "<div class='error'>Image not Available.</div>";
                             }
                             else
                             {
-                                //Image is Available
                                 ?>
                                 <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
                                 <?php
@@ -124,29 +100,24 @@
 
             <?php 
 
-                //CHeck whether submit button is clicked or not
                 if(isset($_POST['submit']))
                 {
-                    // Get all the details from the form
 
                     $food = $_POST['food'];
                     $price = $_POST['price'];
                     $qty = $_POST['qty'];
 
-                    $total = $price * $qty; // total = price x qty 
+                    $total = $price * $qty;
 
-                    $order_date = date("Y-m-d h:i:sa"); //Order DAte
+                    $order_date = date("Y-m-d h:i:sa"); 
 
-                    $status = "Ordered";  // Ordered, On Delivery, Delivered, Cancelled
+                    $status = "Ordered"; 
 
                     $customer_name = $_POST['full-name'];
                     $customer_contact = $_POST['contact'];
                     $customer_email = $_POST['email'];
                     $customer_address = $_POST['address'];
 
-
-                    //Save the Order in Databaase
-                    //Create SQL to save the data
                     $sql2 = "INSERT INTO tbl_order SET 
                         food = '$food',
                         price = $price,
@@ -160,21 +131,16 @@
                         customer_address = '$customer_address'
                     ";
 
-                    //echo $sql2; die();
-
-                    //Execute the Query
                     $res2 = mysqli_query($conn, $sql2);
 
-                    //Check whether query executed successfully or not
                     if($res2==true)
                     {
-                        //Query Executed and Order Saved
+
                         $_SESSION['order'] = "<div class='success text-center'>Food Ordered Successfully.</div>";
                         header('location:'.SITEURL);
                     }
                     else
                     {
-                        //Failed to Save Order
                         $_SESSION['order'] = "<div class='error text-center'>Failed to Order Food.</div>";
                         header('location:'.SITEURL);
                     }
@@ -185,6 +151,6 @@
 
         </div>
     </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+
 
     
