6fad651ee3ebf72d1eff4c453376cc084e35e9f7
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:13:27 +0430

    Update foods.php

--- foods.php
+++ foods.php
@@ -1,22 +1,6 @@
 
     <?php include('partials-front/menu.php'); ?>
 
-    <!-- fOOD sEARCH Section Starts Here 
-    <section class="food-search text-center">
-        <div class="container">
-            
-            <form action="<?php// echo SITEURL; ?>food-search.php" method="POST">
-                <input type="search" name="search" placeholder="Search for Food.." required>
-                <input type="submit" name="submit" value="Search" class="btn btn-primary">
-            </form>
-
-        </div>
-    </section>
-     fOOD sEARCH Section Ends Here -->
-
-
-
-    <!-- fOOD MEnu Section Starts Here -->
     <section class="food-menu">
         <div class="containerr">
             <br>
@@ -26,22 +10,13 @@
             <h2 class="text-center">Food Menu</h2>
 
             <?php 
-                //Display Foods that are Active
                 $sql = "SELECT * FROM tbl_food ";
-
-                //Execute the Query
                 $res=mysqli_query($conn, $sql);
-
-                //Count Rows
                 $count = mysqli_num_rows($res);
-
-                //CHeck whether the foods are availalable or not
                 if($count>0)
                 {
-                    //Foods Available
                     while($row=mysqli_fetch_assoc($res))
                     {
-                        //Get the Values
                         $id = $row['id'];
                         $title = $row['title'];
                         $price = $row['price'];
@@ -51,15 +26,12 @@
                         <div class="food-menu-box">
                             <div class="food-menu-img">
                                 <?php 
-                                    //CHeck whether image available or not
                                     if($image_name=="")
                                     {
-                                        //Image not Available
                                         echo "<div class='error'>Image not Available.</div>";
                                     }
                                     else
                                     {
-                                        //Image Available
                                         ?>
                                         <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" alt="" class="img-responsive img-curve">
                                         <?php
@@ -74,7 +46,6 @@
                                  
                                 <br>
 
-
                                 <a href="<?php echo SITEURL; ?>order.php?food_id=<?php echo $id; ?>" class="btn btn-primary">Order Now</a>
                             </div>
                         </div>
@@ -84,23 +55,15 @@
                 }
                 else
                 {
-                    //Food not Available
                     echo "<div class='error'>Food not found.</div>";
                 }
             ?>
-            
 
-            
 
-            
-
-            <div class="clearfix" >
-
-            
+            <div class="clearfix" >  
 
         </div>
 
     </section>
-    <!-- fOOD Menu Section Ends Here -->
 
  
