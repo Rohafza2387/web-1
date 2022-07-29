cc7f955c051e7c00cb3e3bdf5074872b2229c2eb
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:41:59 +0430

    Update delete-table.php

--- admin/delete-table.php
+++ admin/delete-table.php
@@ -1,1 +1,27 @@
+<?php 
+ 
+    include('../config/constants.php');
 
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM tbl_table WHERE id=$id";
+
+    $res = mysqli_query($conn, $sql);
+
+    if($res==true)
+    {
+
+        $_SESSION['delete'] = "<div class='success'>Deleted Successfully.</div>";
+
+        header('location:'.SITEURL.'admin/manage-table.php');
+    }
+    else
+    {
+
+        $_SESSION['delete'] = "<div class='error'>Failed to Delete</div>";
+        header('location:'.SITEURL.'admin/manage-table.php');
+    }
+
+  
+
+?>
