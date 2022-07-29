8c5047e6e6115dd81f5e800f18d51f3b168a676e
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 16:44:42 +0430

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,30 @@
+
+    <?php include('partials-front/menu.php'); ?>
+
+
+<section id="contact">
+        <img src="http:\\localhost\web\images\depositphoto.jpg">
+        <div class="box">
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br>
+            <h3>contact Us</h3>
+            <p>Address: </p>
+            <p>Email:</p>
+            <p>Phone Number:</p>
+            
+        </div>
+        <div class="box">
+        <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br>
+            <h3>Follow Us</h3>
+             <p>instagram</p>
+             <p>facebook</p>
+             <p>twitter</p>
+        </div>
+
+        
+    </section>
