31af85af4c6eeaba788d4e88c3a5a1792c2c89de
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:52:51 +0430

    Update menu.php

--- admin/partails/menu.php
+++ admin/partails/menu.php
@@ -1,1 +1,32 @@
+<?php 
 
+    include('../config/constants.php'); 
+    include('login-check.php');
+
+?>
+
+
+<html>
+    <head>
+        <title>Online Food Order</title>
+
+        <link rel="stylesheet" href="../css/stylee.css">
+    </head>
+    
+    <body>
+       
+            <div class="navbar">
+                <ul>
+                    <li><a href="index.php">Dashboard</a></li>
+                    <li><a href="manage-food.php">Food Items</a></li>
+                    <li><a href="manage-order.php">Order Section</a></li>
+                    <li><a href="manage-table.php">reservation Section</a></li>
+                    <li><a href="manage-users.php">Manage user</a></li>
+                    <li><a href="manage-admin.php">Manage Admin</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+
+
+
+        
