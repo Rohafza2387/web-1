379b3be4782e62366bcf6a6041782c7da8f75dd3
Hosna12 <78663810+Hosna12@users.noreply.github.com>
2022-07-29 14:21:27 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+    <?php include('partials-front/menu.php'); ?>
+
+
+
+
+    <section id="home">
+        <div>
+            <h3>The Best Food <br>quality</h3>
+            <img src="http://localhost/Restaurant/Imag/54674.jpeg" id=imeg>
+        </div>
+
+               
+            <form method="POST" class="container" action="<?php echo SITEURL; ?>food-search.php">
+                <input type="search" name="search" class="sear" placeholder="Search Foods" required>
+                <input type="submit" name="submit" value="Search" class="btn_pri">
+            </form>
+        
+    </section>
+    
+
+
+
+
